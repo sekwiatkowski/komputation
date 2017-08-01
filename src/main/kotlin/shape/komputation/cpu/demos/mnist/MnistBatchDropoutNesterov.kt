6c2476bcd0c4bb5bc85191992508cd95d8299494
Sebastian E . Kwiatkowski <sebastian@gopher.ai>
2017-08-01 06:58:55 +0200

    - Identified good hyperparameters for the stochastic MNIST demo - Added a mini-batch variation of the MNIST demo

--- src/main/kotlin/shape/komputation/cpu/demos/mnist/MnistBatchDropoutNesterov.kt
+++ src/main/kotlin/shape/komputation/cpu/demos/mnist/MnistBatchDropoutNesterov.kt
@@ -3,7 +3,7 @@ package shape.komputation.cpu.demos.mnist
 import shape.komputation.cpu.Network
 import shape.komputation.cpu.functions.findMaxIndex
 import shape.komputation.demos.mnist.MnistData
-import shape.komputation.initialization.gaussianInitialization
+import shape.komputation.initialization.heInitialization
 import shape.komputation.layers.entry.inputLayer
 import shape.komputation.layers.forward.activation.ActivationFunction
 import shape.komputation.layers.forward.activation.reluLayer
@@ -11,7 +11,7 @@ import shape.komputation.layers.forward.denseLayer
 import shape.komputation.layers.forward.dropout.dropoutLayer
 import shape.komputation.layers.forward.projection.projectionLayer
 import shape.komputation.loss.logisticLoss
-import shape.komputation.optimization.historical.momentum
+import shape.komputation.optimization.historical.nesterov
 import java.io.File
 import java.util.*
 
@@ -25,8 +25,8 @@ fun main(args: Array<String>) {
 
     val random = Random(1)
 
-    val numberIterations = 30
-    val batchSize = 1
+    val numberIterations = 50
+    val batchSize = 64
 
     val (trainingInputs, trainingTargets) = MnistData.loadMnistTraining(File(args.first()))
     val (testInputs, testTargets) = MnistData.loadMnistTest(File(args.last()))
@@ -35,8 +35,8 @@ fun main(args: Array<String>) {
     val hiddenDimension = 100
     val numberCategories = MnistData.numberCategories
 
-    val initialization = gaussianInitialization(random, 0.0f, 0.1f)
-    val optimizer = momentum(0.005f, 0.1f)
+    val initialization = heInitialization(random)
+    val optimizer = nesterov(0.01f, 0.9f)
     val keepProbability = 0.85f
 
     val firstProjection = projectionLayer(
